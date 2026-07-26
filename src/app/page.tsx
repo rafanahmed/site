c5448b08e01e2471ca0f9a2d8edc94662b7416a1
rafanahmed <rahmed7654@gmail.com>
2026-07-26 00:29:30 -0400

    change 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,18 +38,24 @@ export default function Home() {
               <p className="font-mono text-[17px] font-bold tracking-[0.12em] text-foreground/90 sm:text-[22px] sm:tracking-[0.14em] md:text-[26px] lg:text-[30px]">
                 hellow! i am rafan (ra-fawn)!
               </p>
-              <p className="font-(family-name:--font-space-mono) text-[12px] leading-relaxed tracking-[0.02em] text-foreground/60 sm:text-[12px] md:text-[13px]">
-                data engineering intern working on agile delivery automation & applied ai @ truist, based in charlotte, nc, usa
-                <br />
-                <strong>currently learning:</strong>
-                <ul className="list-disc ml-5">
+              <div className="font-(family-name:--font-space-mono) text-[12px] font-normal leading-relaxed tracking-[0.02em] text-foreground/60 sm:text-[12px] md:text-[13px]">
+                <p>
+                  data engineering intern working on agile delivery automation &
+                  applied ai @ truist
+                </p>
+                <p className="my-3">based in charlotte nc</p>
+                <div>
+                  <strong className="font-bold text-foreground/70">
+                    currently learning:
+                  </strong>
+                </div>
+                <ul className="ml-5 list-disc font-normal">
                   <li>ml (nlp, rl)</li>
                   <li>mathematics</li>
                   <li>security 4 ai </li>
                   <li>日本語 !!</li>
                 </ul>
-           
-              </p>
+              </div>
             </div>
           </div>
 
